bdfe5b312e5ce354024280242b47b9f7791aa02d
KalimuenGoat <polodu165@gmail.com>
2023-10-23 15:34:12 +0200

    Ajout du formulaire d'inscription et de connexion

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,17 +2,67 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Form\LoginFormType;
+use App\Form\RegisterFormType;
+
 
 class SecurityController extends AbstractController
 {
-    #[Route('/security', name: 'app_security')]
-    public function index(): Response
+    /**
+     * @Route("/login", name="app_login")
+     */
+    public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        return $this->render('security/index.html.twig', [
-            'controller_name' => 'SecurityController',
+        $error = $authenticationUtils->getLastAuthenticationError();
+        $lastUsername = $authenticationUtils->getLastUsername();
+        $form = $this->createForm(LoginFormType::class);
+
+        return $this->render('security/login.html.twig', [
+            'last_username' => $lastUsername,
+            'error' => $error,
+            'form' => $form
         ]);
     }
+
+    /**
+ * @Route("/register", name="app_register")
+ */
+    public function register(Request $request, UserPasswordHasherInterface $passwordEncoder, EntityManagerInterface $entityManager): Response
+    {
+        // Créer une instance du formulaire d'inscription
+        $form = $this->createForm(RegisterFormType::class);
+
+        // Gérer la soumission du formulaire
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $formData = $form->getData();
+
+            // Créer un nouvel utilisateur
+            $user = new User();
+            $user->setUsername($formData['username']);
+
+            // Encodez et définissez le mot de passe
+            $hashedPassword = $passwordEncoder->hashPassword($user, $formData['password']);
+            $user->setPassword($hashedPassword);
+
+            // Enregistrez l'utilisateur en base de données
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            // Redirigez vers une autre page (par exemple, la page de connexion)
+            return $this->redirectToRoute('app_login');
+        }
+
+        return $this->render('security/register.html.twig', [
+            'form' => $form
+        ]);
+}
 }
