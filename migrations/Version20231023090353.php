3124c3fd6c9a107f130dfd7c39b58cd471a81d2a
KalimuenGoat <polodu165@gmail.com>
2023-10-23 11:55:13 +0200

    Ajout de l'Authentification User

--- migrations/Version20231023090353.php
+++ migrations/Version20231023090353.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20231023085650 extends AbstractMigration
+final class Version20231023090353 extends AbstractMigration
 {
     public function getDescription(): string
     {
