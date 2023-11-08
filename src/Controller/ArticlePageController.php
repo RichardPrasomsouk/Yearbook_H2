bc90d6ed33cdf24b1439c5bc50c04465d73ea518
KalimuenGoat <polodu165@gmail.com>
2023-11-08 14:03:25 +0100

    Modification de la page article

--- src/Controller/ArticlePageController.php
+++ src/Controller/ArticlePageController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ArticlesRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +10,10 @@ use Symfony\Component\Routing\Annotation\Route;
 class ArticlePageController extends AbstractController
 {
     #[Route('/article/page', name: 'app_article_page')]
-    public function index(): Response
+    public function index(ArticlesRepository $articlesRepository): Response
     {
         return $this->render('pages/articles_grid_page/article_grid.html.twig', [
-            'controller_name' => 'ArticlePageController',
+            'articles' => $articlesRepository->findAll(),
         ]);
     }
 }
